ae82fcce30d1cee37f1f85159849aa352d7e1a23
kolug1 <filipjanowski13@gmail.com>
2025-10-29 09:10:23 +0100

    wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+zmienna_1:str= "Filip"
+zmienna_2:str= "Kasia"
+zmienna_3:str= "Krzysiu"
+zmienna_4:str= "Mati"
+
+uzytkownicy:list=[zmienna_1,zmienna_2,zmienna_3,zmienna_4]
+
+for uzytkownik in uzytkownicy:
+    print(f" Hello {uzytkownik}")
