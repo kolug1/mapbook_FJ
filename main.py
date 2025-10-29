77793c3b715937ebcd40f6fbc4b7b1078876f962
kolug1 <filipjanowski13@gmail.com>
2025-10-29 10:36:54 +0100

    wip: added function that prints user info

--- main.py
+++ main.py
@@ -5,5 +5,22 @@ users: list = [
     {"name": "Basia", "location": "Kraśnik", "posts": 88},
 ]
 
-for user in users:
-    print(f"Twój znajomy {user["name"]} z miejscowości {user['location']} opublikował {user["posts"]} postów")
+
+def user_info(users_data: list) -> None:
+    for user in users_data:
+        print(f"Twój znajomy {user["name"]} z miejscowości {user['location']} opublikował {user["posts"]} postów")
+
+
+while True:
+    temp_choice: int = int(input("Wybierz opcję menu "))
+    if temp_choice == 0:
+        break
+    if temp_choice == 1:
+        print("Wybrano funkcję wyświetlania aktywności znajomych")
+        user_info(users)
+    if temp_choice == 2:
+        print("Wybrano funkcję dodawania znajomego")
+    if temp_choice == 3:
+        print("Wybrano funkcję usuwania znajomego")
+    if temp_choice == 4:
+        print("Wybrano funkcję aktualizacji danych znajomego")
